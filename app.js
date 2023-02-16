941a585cc004b42b9321c1609b6f70db25b2d727
Emaz Akhtar <emazakhtar11@gmail.com>
2023-02-17 03:19:53 +0530

    Added Envirionment Vars

---
+++ app.js
@@ -0,0 +1,95 @@
+require('dotenv').config();
+const express= require("express");
+const bodyParser= require("body-parser");
+const ejs= require("ejs");
+const mongoose= require("mongoose");
+const encrypt= require("mongoose-encryption");
+
+const app= express();
+
+app.use(express.static("public"));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+mongoose.connect("mongodb://127.0.0.1:27017/usersDB");
+
+// make a proper mongoose schema inorder to use a plugin on the schema 
+const usersSchema =new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+// secret key 
+
+// plugging in mongoose encryptor into the users schema 
+usersSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:["password"]});
+
+
+const User= mongoose.model("User",usersSchema);
+
+app.get("/",function(req,res){
+    res.render("home");
+});
+app.get("/register",function(req,res){
+    res.render("register");
+});
+app.get("/login",function(req,res){
+    res.render("login");
+});
+
+app.post("/register",function(req,res){
+
+    const newUser= new User({
+        email:req.body.username,
+        password: req.body.password
+        
+    });
+    newUser.save(function(err){
+        if(err){
+            console.log(err)
+        }
+        else{
+            res.render("secrets");
+        }
+    });
+});
+
+app.post("/login",function(req,res){
+
+    const userEmail= req.body.username;
+    const userPassword= req.body.password;
+
+    User.findOne({email:userEmail},function(err,foundUser){
+         if(err){
+            console.log(err);
+         }
+         else{
+            if(foundUser){
+            if(foundUser.password === userPassword){
+                res.render("secrets");
+            }
+        }
+    }
+            
+    });
+    const newUser= new User({
+        email:req.body.username,
+        password: req.body.password
+        
+    });
+    newUser.save(function(err){
+        if(err){
+            console.log(err)
+        }
+        else{
+            res.render("secrets");
+        }
+    });
+});
+
+
+
+
+app.listen(3000,function(){
+    console.log("server started at port 3000");
+});
